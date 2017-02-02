a84dc2b3c3400c9394745d7f3bfcf87ad14497d5
Stephen1190 <stephen_schneider@gmx.de>
2017-02-02 01:06:23 +0100

    add level 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieComponent } from './movie/movie.component';
 import { GenresComponent } from './genres/genres.component';
-import { UpcomingComponent } from './upcoming/upcoming.component';
 import { PopularSeriesComponent } from './popular-series/popular-series.component';
 import { SerieComponent } from './serie/serie.component';
 import { ActorComponent } from './actor/actor.component';
@@ -13,6 +12,9 @@ export const appRoutes: Routes = [
     {path: 'tv/:id', component: SerieComponent},
     {path: 'actor/:id', component: ActorComponent},
     {path: 'genres/:id/:name', component: GenresComponent},
-    {path: 'upcoming', component: UpcomingComponent},
+    {path: 'upcoming', component: PopularSeriesComponent}, //hmmm do we really need 2 PopularSeriesComponents? I think 1 is enough
+                                                            //let's find a better one for 'upcoming'
+                                                            //I wonder if I can find it in the upcoming folder?
+                                                            //noo, that would be too obvious...
     {path: 'popular/series', component: PopularSeriesComponent},
 ];
